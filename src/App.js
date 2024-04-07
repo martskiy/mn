698ba2b55603c8588c37183108e7169aa662abe4
gneqstr <bb704805@gmail.com>
2024-04-07 18:46:07 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ClaimPage from './pages/ClaimPage';
+import Boost from './pages/Boost';
+import Task from './pages/Task';
+import { UserProvider } from './context/UserContext'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <UserProvider >
+          <Route component={HomePage} path="/" exact />
+          <Route component={ClaimPage} path="/claim" exact />
+          <Route component={Boost} path="/boost" exact />
+          <Route component={Task} path="/task" exact />
+          </UserProvider>
+        </Switch>
+      </Router>
     </div>
   );
 }
